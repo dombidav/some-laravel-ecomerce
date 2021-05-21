53ffbc99bc5659b3b850e7a61f9316d90f587e38
Dombi Tibor <dombidav@gmail.com>
2021-05-21 08:27:35 +0200

    Util: Added Description for RequestMap::nameToID function

--- app/Utils/RequestMap.php
+++ app/Utils/RequestMap.php
@@ -10,6 +10,13 @@ use Illuminate\Support\Str;
 
 class RequestMap
 {
+    /**
+     * Remaps some input to a model id. For example if you have a `category` input taking the NAME of the category, but you need to store the category->id, this method will replace `category` with `category_id` and with the appropriate ID as value. Every other input field will be passed as is
+     * @param FormRequest $request The Haystack. Request to validate.
+     * @param string $model The Subject. The model we need to replace
+     * @param string $byColumn The column that should be used for search
+     * @return array Replaced and validated array
+     */
     public static function nameToID(FormRequest $request, string $model, string $byColumn = 'name'): array
     {
         $snake = Str::of($model)->afterLast('\\')->snake();
